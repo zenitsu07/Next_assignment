d7e6dd74ba2b87ff549f0af7083c5f44baf14d81
Utkarsh Srivastava <utkarsh.dev83@gmail.com>
2023-12-19 22:52:21 +0530

    middleware update

--- app/api/_middleware.js
+++ app/api/_middleware.js
@@ -1,26 +1,22 @@
 import mongoose from "mongoose";
-const connection = mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-  connection.on('connected', () => {
-    console.log('MongoDB connected for API routes!');
-  });
-  
-  connection.on('error', (err) => {
-    console.error('Error connecting to MongoDB:', err);
-  });
 
-  const connectMiddleware = (req, res, next) => {
-    if (!connection.readyState) {
-      connection.once('connected', next);
-      connection.once('error', () => {
-        console.error('MongoDB connection is not ready.');
-        res.status(500).json({ error: 'Internal Server Error' });
+const connectMiddleware = async (req, res, next) => {
+  try {
+    if (mongoose.connection.readyState === 0) {
+      const connection = await mongoose.connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
       });
-    } else {
-      next();
-    }
-  };
 
-export { connection, connectMiddleware };
+      connection.connection.on('connected', () => {
+        console.log('MongoDB connected for API routes!');
+      });
+    }
+    next();
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export default connectMiddleware;
